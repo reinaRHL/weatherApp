4c88aaf74a15f803edaeadb64a318d8f082c3a56
reinal <ff14purinnnn@gmail.com>
2018-12-11 15:17:12 -0800

    Added appID

--- app/src/main/java/com/wa/rena/weatherapp/MainActivity.java
+++ app/src/main/java/com/wa/rena/weatherapp/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String appid = "18c51bde0a28d9196fef71382fcbd4d8";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
+
+//example api call
+//http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=18c51bde0a28d9196fef71382fcbd4d8
