651fd0cd09716a1ead1988c51b7d20750c928c82
reinal <ff14purinnnn@gmail.com>
2018-12-11 16:43:50 -0800

    Added DownloadTask class that connects to the website and downloads queried weather information

--- app/src/main/java/com/wa/rena/weatherapp/MainActivity.java
+++ app/src/main/java/com/wa/rena/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wa.rena.weatherapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     String appid = "18c51bde0a28d9196fef71382fcbd4d8";
@@ -16,6 +27,61 @@ public class MainActivity extends AppCompatActivity {
     EditText cityTextView;
     TextView weatherTextView;
 
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result ="";
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream stream = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(stream);
+
+                int data = reader.read();
+
+                while (data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jasonObj = new JSONObject(result);
+                String info = jasonObj.getString("weather");
+                JSONArray arr = new JSONArray(info);
+
+                for (int i=0; i < arr.length(); i++){
+                    JSONObject infoJson = arr.getJSONObject(i);
+                    Log.i("info", infoJson.getString("main"));
+                    Log.i("info", infoJson.getString("description"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+    public void findWeather(){
+        String city = cityTextView.getText().toString();
+
+        DownloadTask task = new DownloadTask();
+        String urlString = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + appid;
+        task.execute(urlString);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         searchBtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("test", "test");
+                findWeather();
             }
         });
     }
