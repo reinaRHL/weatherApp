cbc87314a0ed3f47d262b2b17eba688da35de28e
reinal <ff14purinnnn@gmail.com>
2018-12-11 16:14:29 -0800

    Added listener to the search button

--- app/src/main/java/com/wa/rena/weatherapp/MainActivity.java
+++ app/src/main/java/com/wa/rena/weatherapp/MainActivity.java
@@ -2,15 +2,37 @@ package com.wa.rena.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     String appid = "18c51bde0a28d9196fef71382fcbd4d8";
+    Button searchBtn;
+    EditText cityTextView;
+    TextView weatherTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchBtn = findViewById(R.id.searchBtn);
+        cityTextView = findViewById(R.id.cityEditText);
+        weatherTextView = findViewById(R.id.weatherDescriptionTextView);
+
+        searchBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Log.i("test", "test");
+            }
+        });
+
     }
 }
 
